d6279476e627f9be480a8d28b412f0f3594177c8
ghassenHamza <ghassenhamza2@gmail.com>
2026-07-20 20:56:27 +0100

    feat(customer): add Customer entity with company relationship

--- Backend/src/main/java/tn/ghassen/Inventory/InventoryApplication.java
+++ Backend/src/main/java/tn/ghassen/Inventory/InventoryApplication.java
@@ -1,4 +1,4 @@
-package tn.ghassen.Inventory;
+package tn.ghassen.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Backend/src/main/java/tn/ghassen/Inventory/entity/Customer.java
+++ Backend/src/main/java/tn/ghassen/Inventory/entity/Customer.java
@@ -1,4 +1,41 @@
 package tn.ghassen.inventory.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String phone;
+
+    private String taxNumber;
+
+    private String address;
+
+    private String logo;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
 }
