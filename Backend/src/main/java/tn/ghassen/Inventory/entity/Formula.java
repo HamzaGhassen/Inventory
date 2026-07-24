2012fc3081aa9004912ebd1ff93e48b1b3a4fa7f
ghassenHamza <ghassenhamza2@gmail.com>
2026-07-24 22:26:23 +0100

    feat(formula): implement Formula and FormulaItem entities

--- Backend/src/main/java/tn/ghassen/Inventory/entity/Formula.java
+++ Backend/src/main/java/tn/ghassen/Inventory/entity/Formula.java
@@ -1,4 +1,26 @@
 package tn.ghassen.inventory.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public class Formula {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "formulas")
+public class Formula extends BaseEntity {
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @OneToMany(mappedBy = "formula")
+    private List<FormulaItem> formulaItems = new ArrayList<>();
 }
--- Backend/src/main/java/tn/ghassen/Inventory/entity/FormulaItem.java
+++ Backend/src/main/java/tn/ghassen/Inventory/entity/FormulaItem.java
@@ -1,4 +1,30 @@
 package tn.ghassen.inventory.entity;
 
-public class FormulaItem {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name ="formula_items")
+public class FormulaItem extends BaseEntity {
+
+    @Column(nullable = false)
+    private BigDecimal quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "raw_material_id", nullable = false)
+    private RawMaterial rawMaterial;
+
+    @ManyToOne
+    @JoinColumn(name = "formula_id",nullable = false)
+    private Formula formula;
+
 }
