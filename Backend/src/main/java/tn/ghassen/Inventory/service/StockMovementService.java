b1b03e706176418efc1d86b903c96d5b90b1f349
ghassenHamza <ghassenhamza2@gmail.com>
2026-08-06 00:13:43 +0100

    feat(StockMovement):Implement Service CRUD opêrations

--- Backend/src/main/java/tn/ghassen/Inventory/service/StockMovementService.java
+++ Backend/src/main/java/tn/ghassen/Inventory/service/StockMovementService.java
@@ -1,4 +1,19 @@
 package tn.ghassen.inventory.service;
 
+import tn.ghassen.inventory.entity.StockMovement;
+
+import java.util.List;
+
 public interface StockMovementService {
+
+    StockMovement createStockMovement(StockMovement stockMovement);
+
+    StockMovement getStockMovementById(Long id);
+
+    List<StockMovement> getAllStockMovements();
+
+    StockMovement updateStockMovement(Long id , StockMovement stockMovement);
+
+    void deleteStockMovement(Long id);
+
 }
--- Backend/src/main/java/tn/ghassen/Inventory/service/impl/StockMovementServiceImpl.java
+++ Backend/src/main/java/tn/ghassen/Inventory/service/impl/StockMovementServiceImpl.java
@@ -1,4 +1,51 @@
 package tn.ghassen.inventory.service.impl;
 
-public class StockMovementServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.ghassen.inventory.entity.StockMovement;
+import tn.ghassen.inventory.repository.StockMovementRepository;
+import tn.ghassen.inventory.service.StockMovementService;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class StockMovementServiceImpl implements StockMovementService {
+
+    private final StockMovementRepository stockMovementRepository;
+    @Override
+    public StockMovement createStockMovement(StockMovement stockMovement) {
+        return stockMovementRepository.save(stockMovement);
+    }
+
+    @Override
+    public StockMovement getStockMovementById(Long id) {
+        return stockMovementRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Stock_movement not found"));
+    }
+
+    @Override
+    public List<StockMovement> getAllStockMovements() {
+        return stockMovementRepository.findAll();
+    }
+
+    @Override
+    public StockMovement updateStockMovement(Long id, StockMovement stockMovement) {
+
+        StockMovement existing = getStockMovementById(id);
+
+        existing.setQuantity(stockMovement.getQuantity());
+        existing.setAction(stockMovement.getAction());
+        existing.setDescription(stockMovement.getDescription());
+
+
+
+        return stockMovementRepository.save(existing);
+    }
+
+    @Override
+    public void deleteStockMovement(Long id) {
+    stockMovementRepository.deleteById(id);
+    }
 }
