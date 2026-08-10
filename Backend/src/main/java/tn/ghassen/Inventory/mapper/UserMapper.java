53428c86bde33586baeb0e4e987aa31b1937a09b
ghassenHamza <ghassenhamza2@gmail.com>
2026-08-10 02:47:38 +0100

    feat(user): add user mapper

--- Backend/src/main/java/tn/ghassen/Inventory/mapper/UserMapper.java
+++ Backend/src/main/java/tn/ghassen/Inventory/mapper/UserMapper.java
@@ -1,4 +1,47 @@
 package tn.ghassen.inventory.mapper;
 
+import org.springframework.stereotype.Component;
+import tn.ghassen.inventory.dto.user.UserCreateDTO;
+import tn.ghassen.inventory.dto.user.UserResponseDTO;
+import tn.ghassen.inventory.dto.user.UserUpdateDTO;
+import tn.ghassen.inventory.entity.User;
+
+@Component
 public class UserMapper {
+
+    public User toEntity(UserCreateDTO dto) {
+        User user = new User();
+
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setPhone(dto.getPhone());
+        user.setRole(dto.getRole());
+        user.setStatus(dto.getEmployeeStatus());
+
+        return user;
+    }
+
+    public UserResponseDTO toResponseDTO(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+
+        dto.setId(user.getId());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        dto.setEmail(user.getEmail());
+        dto.setPhone(user.getPhone());
+        dto.setRole(user.getRole());
+        dto.setEmployeeStatus(user.getStatus());
+        dto.setCreatedAt(user.getCreatedAt());
+        dto.setUpdatedAt(user.getUpdatedAt());
+
+        return dto;
+    }
+
+    public void updateEntity(User user, UserUpdateDTO dto) {
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setPhone(dto.getPhone());
+    }
 }
