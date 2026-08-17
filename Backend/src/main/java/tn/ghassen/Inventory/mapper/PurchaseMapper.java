3e122cb056973fd66ac6bf510db17df44f34304d
ghassenHamza <ghassenhamza2@gmail.com>
2026-08-17 20:26:25 +0100

    refactor(Purcahse):add List<PurchaseItemsDTO> in Response method;Delete Unused attributes;add Wrapper method.

--- Backend/src/main/java/tn/ghassen/Inventory/mapper/PurchaseMapper.java
+++ Backend/src/main/java/tn/ghassen/Inventory/mapper/PurchaseMapper.java
@@ -1,16 +1,27 @@
 package tn.ghassen.inventory.mapper;
 
 import org.springframework.stereotype.Component;
+import tn.ghassen.inventory.dto.PurchaseItem.PurchaseItemResponseDTO;
 import tn.ghassen.inventory.dto.Purchase.PurchaseCreateDTO;
 import tn.ghassen.inventory.dto.Purchase.PurchaseResponseDTO;
 import tn.ghassen.inventory.dto.Purchase.PurchaseUpdateDTO;
 import tn.ghassen.inventory.entity.Purchase;
 
+import java.util.Collections;
+import java.util.List;
+
 @Component
 public class PurchaseMapper {
 
-    public Purchase toEntity(PurchaseCreateDTO dto) {
+private final PurchaseItemMapper purchaseItemMapper;
+    public PurchaseMapper(PurchaseItemMapper purchaseItemMapper) {
+        this.purchaseItemMapper = purchaseItemMapper;
+    }
 
+    public Purchase toEntity(PurchaseCreateDTO dto) {
+        if (dto == null) {
+            return null;
+        }
         Purchase purchase = new Purchase();
 
         purchase.setTotalAmount(dto.totalAmount());
@@ -30,16 +41,22 @@ public class PurchaseMapper {
     }
 
     public PurchaseResponseDTO toResponseDTO(Purchase purchase) {
-
+        List<PurchaseItemResponseDTO> items = purchase.getPurchaseItems() != null
+                ? purchase.getPurchaseItems().stream()
+                .map(purchaseItemMapper::toResponseDTO)
+                .toList()
+                : Collections.emptyList();
         return new PurchaseResponseDTO(
                 purchase.getId(),
-                purchase.getSupplier().getId(),
-                purchase.getUser().getId(),
-                purchase.getCompany().getId(),
+                purchase.getSupplier() != null ? purchase.getSupplier().getId() : null,
+                purchase.getUser() != null ? purchase.getUser().getId(): null,
+                purchase.getCompany() != null ? purchase.getCompany().getId():null,
                 purchase.getTotalAmount(),
+                items,
                 purchase.getPaymentStatus(),
                 purchase.getPaymentMethod(),
                 purchase.getCreatedAt()
         );
     }
+    public  PurchaseResponseDTO toResponse(Purchase purchase){return toResponseDTO(purchase);}
 }
