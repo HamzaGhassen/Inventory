b871cb7e5b71d240398764d2a4743e2d54099f6f
ghassenHamza <ghassenhamza2@gmail.com>
2026-08-20 23:58:22 +0100

    feat(sale_item):add sale_item mapper

--- Backend/src/main/java/tn/ghassen/Inventory/mapper/SaleItemMapper.java
+++ Backend/src/main/java/tn/ghassen/Inventory/mapper/SaleItemMapper.java
@@ -1,4 +1,51 @@
 package tn.ghassen.inventory.mapper;
 
+import org.springframework.stereotype.Component;
+import tn.ghassen.inventory.dto.Sale_Item.SaleItemCreateDTO;
+import tn.ghassen.inventory.dto.Sale_Item.SaleItemResponseDTO;
+import tn.ghassen.inventory.dto.Sale_Item.SaleItemUpdateDTO;
+import tn.ghassen.inventory.entity.SaleItem;
+
+
+@Component
 public class SaleItemMapper {
+    public SaleItem toEntity(SaleItemCreateDTO dto){
+        SaleItem item = new SaleItem();
+        item.setQuantity(dto.quantity());
+        item.setUnitPrice(dto.unitPrice());
+        item.setDiscount(dto.discount());
+        item.setTax(dto.tax());
+
+        return item;
+    }
+    public void updateEntity(SaleItem item , SaleItemUpdateDTO dto){
+        if (dto.quantity()!=null) {
+            item.setQuantity(dto.quantity());
+        }
+        if (dto.unitPrice()!=null) {
+            item.setUnitPrice(dto.unitPrice());
+        }
+        if(dto.discount()!=null) {
+            item.setDiscount(dto.discount());
+        }
+        if (dto.tax()!=null) {
+            item.setTax(dto.tax());
+        }
+
+    }
+    public SaleItemResponseDTO toResponseDTO(SaleItem item){
+        return  new SaleItemResponseDTO(
+                item.getId(),
+                item.getProduct()!=null ? item.getProduct().getId() : null,
+                item.getSale()!=null ? item.getSale().getId() : null ,
+                item.getQuantity(),
+                item.getUnitPrice(),
+                item.getTotalPrice(),
+                item.getDiscount(),
+                item.getTax(),
+                item.getSubtotal(),
+                item.getCreatedAt(),
+                item.getUpdatedAt()
+        );
+    }
 }
